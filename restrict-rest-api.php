b0eefe74eab91c8a9573dd7a4f3620bb5f8e7963
stevennoad <110924473+stevennoad@users.noreply.github.com>
2024-10-19 19:01:32 +0100

    V1.0.1

--- restrict-rest-api.php
+++ restrict-rest-api.php
@@ -3,7 +3,7 @@
 Plugin Name: Restrict REST API Access
 Plugin URI:  https://github.com/stevennoad/restrict-wordpress-rest-api
 Description: Restricts access to the WordPress REST API to authenticated users only.
-Version:     1.0.0
+Version:     1.0.1
 Author:      Steve Noad
 License:     MIT
 Text Domain: restrict-wordpress-rest-api
@@ -16,7 +16,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add the filter to restrict REST API access
 add_filter( 'rest_authentication_errors', function( $result ) {
-  if ( ! is_user_logged_in() ) {
+  if (true === $result || is_wp_error($result)) {
+		return $result;
+	}
+
+	if ( ! is_user_logged_in() ) {
     return new WP_Error( 'Unauthorized', 'Authentication credentials are missing or invalid. Please provide a valid API token.', array( 'status' => 401 ) );
   }
   return $result;
